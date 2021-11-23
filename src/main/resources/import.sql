9e0c9f8dff29ddc2bc65617cca1e25ac1d266308
Carlos Daiha Basna <cdaihaba@LES008853.corp.capgemini.com>
2021-11-23 13:42:44 +0100

    Importación de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Jose','Perez','77074504L','jp@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Carlos','Lopez','77074505B','cl@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Maria','Orillana','77074507H','mo@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Dina','Ramirez','907074504L','dr@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Mirna','Ramos','77784504A','mr@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Pepe','Mojica','71924504L','pm@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Juan','Chavez','770925704L','jc@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Enrrique','Iglesias','792749K','ei@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Pedro','Diaz','728490180L','pd@hotmail.com',65433223,'Calle Capitán','28300');
+INSERT INTO alumnos (nombre,apellido,dni,email,telefono,direccion,codigo_postal) VALUES('Ramon','Gonzalez','77074745C','rgonzalez@gmail.com',65433223,'Calle Capitán','28300');
