b6a1b93a3a8c1826c65de55647e740ca5a65a29d
Carlos Daiha Basna <cdaihaba@LES008853.corp.capgemini.com>
2021-11-23 19:15:30 +0100

    Anotación CrossOrigin en Controller

--- src/main/java/com/springboot/apirest/alumnos/controllers/AlumnoRestController.java
+++ src/main/java/com/springboot/apirest/alumnos/controllers/AlumnoRestController.java
@@ -18,13 +18,14 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,7 +34,7 @@ import com.springboot.apirest.alumnos.models.entity.Alumno;
 import com.springboot.apirest.alumnos.services.AlumnoService;
 
 @RestController
-@RequestMapping("/clase")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class AlumnoRestController implements ControllerDoc {
 
 	@Autowired
